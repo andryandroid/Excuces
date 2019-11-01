22a249c098fd64511ed59a50aa52d378566ef836
andryandroid <andryandroid@users.noreply.github.com>
2019-11-01 13:43:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 var frase = {
-    who: ['the dog','my granma','his turtle','my bird'], 
-    what: [ 'eat','pissed','crushed','broked'], 
-    object: ['my homework','my car','sandwich','my computer'], 
-    when: [ 'before the class','right in time','when I finished','during my lunch','while I was praying'] 
-    }; 
+    who: ['the dog','my granma','his turtle','my bird'],
+    what: [ 'ate','pissed','crashed','broked'],
+    object: ['my homework','my car','my sandwich','my computer'],
+    when: [ 'before the class','5 minutes ago','when I finished it','during my lunch','while I was praying']
+    };
 
 function random(max){
- document.write(max);
-} 
-
-function generar(){    
-    document.write("<h1 id='excuse'>Mi perro se comió la tarea</h1>");
-    random(2);
+    var num = Math.floor(Math.random()*max);
+    return num;
 }
+
+function generar(){
+    document.write("<h1>"+frase.who[random(frase.who.length)]+" ");
+    document.write(frase.what[random(frase.what.length)]+" ");
+    document.write(frase.object[random(frase.object.length)]+" ");
+    document.write(frase.when[random(frase.when.length)]+"</h1>");
+}
+
