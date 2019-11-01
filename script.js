18a9f1a8c9b94d72fe51f00558d1198bd93b15fd
andryandroid <andryandroid@users.noreply.github.com>
2019-11-01 14:13:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var frase = {
     who: ['the dog','my granma','his turtle','my bird'],
     what: [ 'ate','pissed','crashed','broked'],
     object: ['my homework','my car','my sandwich','my computer'],
-    when: [ 'before the class','5 minutes ago','when I finished it','during my lunch','while I was praying']
+    when: [ 'before the class','this morning','when I finished it','during my lunch','while I was praying']
     };
 
 function random(max){
@@ -11,7 +11,10 @@ function random(max){
 }
 
 function generar(){
-    document.write("<h1>"+frase.who[random(frase.who.length)]+" ");
+
+    document.write("<h1> OMG! you will not believe me but...</h1>");
+
+    document.write("<h1 id='excuse'>"+frase.who[random(frase.who.length)]+" ");
     document.write(frase.what[random(frase.what.length)]+" ");
     document.write(frase.object[random(frase.object.length)]+" ");
     document.write(frase.when[random(frase.when.length)]+"</h1>");
