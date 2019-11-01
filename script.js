7599cb79e155f2ffb57475e48c346a735d97fbab
andryandroid <andryandroid@users.noreply.github.com>
2019-11-01 12:44:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("hola");
+var frase = {
+    who: ['the dog','my granma','his turtle','my bird'], 
+    what: [ 'eat','pissed','crushed','broked'], 
+    object: ['my homework','my car','sandwich','my computer'], 
+    when: [ 'before the class','right in time','when I finished','during my lunch','while I was praying'] 
+    }; 
+
+function random(max){
+ document.write(max);
+} 
+
+function generar(){    
+    document.write("<h1 id='excuse'>Mi perro se comió la tarea</h1>");
+    random(2);
+}
